c8c98104ab53830cc6f99714afd95904c9243fc2
hanstoj <stojack.hannah@gmail.com>
2021-01-05 21:19:56 -0800

    JS interest Container function created

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,1 +1,14 @@
-<!DOCTYPE html>
+
+function interestContainer(props) {
+    const interests = [];
+
+    for (const currentInterest of interestData) {
+      interests.push(
+        <Interest
+          name={currentInterest.name}
+          interestLevel={currentInterest.interestLevel}
+          imgUrl={currentInterest.imgUrl}
+
+        />
+      );
+    }
